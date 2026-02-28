b17a21baecea8774df7aba73f5b4bdc9e447b921
TheTechApostle <wonderpaul246@gmail.com>
2026-02-28 19:23:00 +0100

    I created Organization Model

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -17,3 +17,12 @@ class Tenant(models.Model):
 
 def __str__(self):
     return f'{self.name}'
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f'{self.name}'
